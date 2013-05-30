c24545b14f2e210f45369b75a77e8a6cd5fffa13
lijianweiDboy <417574637@qq.com>
2013-05-30 13:18:32 +0800

    ljw

---
+++ CK_PluginOrder/FormProductApproveReport.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace CK_PluginOrder
+{
+    partial class FormProductApproveReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormProductApproveReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 394);
+            this.Name = "FormProductApproveReport";
+            this.Text = "采购单审批";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
